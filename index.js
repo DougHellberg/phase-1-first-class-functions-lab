fa2193457d9f39b6a34d590987e0ab22f57cebd1
Douglas Hellberg <DougHellberg@gmail.com>
2023-07-11 16:38:48 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    const twoDrivers = [];
+    
+    for(let i=0;i<2;i++){
+    twoDrivers.push(drivers[i])
+    }
+
+    return twoDrivers
+}
+
+function returnLastTwoDrivers(drivers){
+    const twoDrivers = [];
+    return drivers.slice(2)
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    function fareMultiplier(fare){
+        fare = fare*multiplier
+        return fare
+    }
+    return fareMultiplier
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers,returnDrivers){
+    return returnDrivers(drivers)
+}
